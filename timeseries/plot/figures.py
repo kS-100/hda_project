e1067bbbda5e3f31a7ed3ae60d5ebfd123adabf6
Karen <Karen.Schulz@stud.h-da.de>
2020-02-21 12:21:45 +0100

    Add function for plotting of mult. ts in same figure

--- timeseries/plot/figures.py
+++ timeseries/plot/figures.py
@@ -4,22 +4,44 @@ import seaborn as sns
 import pandas as pd
 
 
-def plot_timeseries(dataframe, title_addition = "", savePath = "", units = "value"):
+def plot_timeseries_single(dataframe, title_addition = "", savePath = "", units = "value"):
     """
     Generates one plot for each column in dataframe
     If savePath is not modified, plots will not be saved; else saved as pdf
     """
+    dataframe = pd.DataFrame(dataframe)
     if units == "value":
-        units = ["value"]*dataframe.shape[1] 
+        units = ["value"]*dataframe.shape[1]
     for index, col in enumerate(dataframe.columns):
-        plt.figure(figsize=(25, 5))
+        plt.figure(figsize=(20, 5))
         g=sns.set(style='white',font_scale=1.5)
         g = sns.lineplot(data=dataframe[col])
         sns.despine()
-        g.set(xlabel='Date', ylabel=units[index] ,title=col + " " + title_addition)
+        g.set(xlabel='Date', ylabel=units[index], title=col + " " + title_addition)
         if savePath != "":
             g.figure.savefig(savePath + "ts_" + col + ".pdf")
-            
+
+
+def plot_timeseries_multiple(dataframe, title_addition="", savePath="", units="value"):
+    """
+    Generates one plot for all columns in dataframe
+    If savePath is not modified, plots will not be saved; else saved as pdf
+    """
+    dataframe = pd.DataFrame(dataframe)
+    if units == "value":
+        units = ["value"] * dataframe.shape[1]
+
+    plt.figure(figsize=(20, 5))
+    g = sns.set(style='white', font_scale=1.5)
+    for index, col in enumerate(dataframe.columns):
+        g = sns.lineplot(data=dataframe[col], label=col)
+    sns.despine()
+    g.legend()
+    g.set(xlabel='Date', ylabel=units[index], title="Comparison" + " " + title_addition)
+    if savePath != "":
+        g.figure.savefig(savePath + "ts_Comparison" + col + title_addition + ".pdf")
+
+
 def plot_distribution(dataframe, title_addition = "", savePath = "", units = "value"):
     """
     Generates one plot for each column in dataframe
