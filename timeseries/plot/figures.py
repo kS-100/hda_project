cd8b97391363f23423343cb980dd15f58c9f12cd
Karen <Karen.Schulz@stud.h-da.de>
2020-02-22 01:07:58 +0100

    Add xlim to dist plot fct

--- timeseries/plot/figures.py
+++ timeseries/plot/figures.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import pandas as pd
 
 
-def plot_timeseries_single(dataframe, title_addition = "", savePath = "", units = "value"):
+def plot_timeseries_single(dataframe, title_addition="", savePath="", units="value"):
     """
     Generates one plot for each column in dataframe
     If savePath is not modified, plots will not be saved; else saved as pdf
@@ -14,7 +14,7 @@ def plot_timeseries_single(dataframe, title_addition = "", savePath = "", units 
         units = ["value"]*dataframe.shape[1]
     for index, col in enumerate(dataframe.columns):
         plt.figure(figsize=(20, 5))
-        g=sns.set(style='white',font_scale=1.5)
+        g = sns.set(style='white', font_scale=1.5)
         g = sns.lineplot(data=dataframe[col])
         sns.despine()
         g.set(xlabel='Date', ylabel=units[index], title=col + " " + title_addition)
@@ -42,7 +42,7 @@ def plot_timeseries_multiple(dataframe, title_addition="", savePath="", units="v
         g.figure.savefig(savePath + "ts_Comparison" + col + title_addition + ".pdf")
 
 
-def plot_distribution(dataframe, title_addition = "", savePath = "", units = "value"):
+def plot_distribution(dataframe, title_addition="", savePath="", units="value", xlim=""):
     """
     Generates one plot for each column in dataframe
     If savePath is not modified, plots will not be saved; else saved as pdf
@@ -51,10 +51,13 @@ def plot_distribution(dataframe, title_addition = "", savePath = "", units = "va
     if units == "value":
         units = ["value"]*dataframe.shape[1] 
     for index, col in enumerate(dataframe.columns):
-        g=sns.set(style='white',font_scale=1.5)
+        g = plt.gcf().subplots_adjust(bottom=0.15)
+        g = sns.set(style='white',font_scale=1.5)
         g = sns.distplot(dataframe[col].dropna(axis = 0), hist = True, norm_hist = True)
         sns.despine()
-        g.set(xlabel=units[index], ylabel= "rate" ,title="Distribution of " + col + " " + title_addition)
+        g.set(xlabel=units[index], ylabel="rate", title="Distribution of " + col + " " + title_addition)
+        if xlim != "":
+            plt.xlim(xlim[0], xlim[1])
         plt.show()
         if savePath != "":
-            g.figure.savefig(savePath + "ts_" + col + ".pdf")
+            g.figure.savefig(savePath + "ts_" + col + "_" + title_addition + ".pdf")
